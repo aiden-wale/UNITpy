a313d47cce41be94fdb0c8cde4c333bba055ba26
Aiden Wale <aiden.wale@gmail.com>
2025-07-13 22:35:45 +1000

    modify docgen_m2html.py to also generate graph

--- docgen_m2html.py
+++ docgen_m2html.py
@@ -1,4 +1,5 @@
 import sys
+#import subprocess
 
 def docgen_m2html_run(path_m2html, path_unit):
     print("\nImporting MATLAB engine into environment... ", end="")
@@ -12,12 +13,22 @@ def docgen_m2html_run(path_m2html, path_unit):
     matlabEng.addpath(path_unit, nargout=0)
     print("done", end="")
     print("\n")
+
+    docDirName = "doc"
     
-    print("\nm2html: Generating documentation for "+path_unit+"... ", end="")
+    print("\nm2html: Generating documentation for "+path_unit+"...\n ", end="")
     matlabEng.workspace['path_unit']    = path_unit
-    matlabEng.workspace['path_doc']     = "doc"
-    matlabEng.eval("m2html('mfiles', path_unit, 'htmldir', path_doc);", nargout=0)
-    print("done", end="")
+    matlabEng.workspace['path_doc']     = docDirName
+    matlabEng.eval("m2html('mfiles', path_unit, 'htmldir', path_doc, 'graph', 'on');", nargout=0)
+    print("\nm2html: Finished generating documentation", end="")
+    # print("\ngraphviz: Creating graph.pdf from dot file", end="")
+    # print("\n")
+    # bashcmd = "dot -Tpdf "+ docDirName + "/matlab/unit/graph.dot -o " + docDirName + "/matlab/unit/graph.pdf"
+    # process = subprocess.Popen(bashcmd.split(), stdout=subprocess.PIPE)
+    # output, error = process.communicate()
+    # if error:
+    #     raise Exception("ERROR OCCURED DURING GRAPH GENERATION")
+    # print(output.decode())
     print("\n")
 
     matlabEng.quit()
